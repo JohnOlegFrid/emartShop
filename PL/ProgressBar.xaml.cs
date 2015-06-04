a724c559da41301eef7b9dbf67499e9c7ee7d1dc
arielboss <tobiz7@hotmail.com>
2015-06-04 13:01:22 +0300

    progress bar arranged

--- PL/ProgressBar.xaml.cs
+++ PL/ProgressBar.xaml.cs
@@ -24,11 +24,13 @@ namespace PL
     public partial class ProgressBar : Window
     {
         BL_Manager bl_manager;
+        Boolean finished = false;
 
         public ProgressBar(BL_Manager bl_manager)
         {
             InitializeComponent();
             this.bl_manager = bl_manager;
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
         }
 
         private void Window_ContentRendered(object sender, EventArgs e)
@@ -43,12 +45,14 @@ namespace PL
 
         void worker_DoWork(object sender, DoWorkEventArgs e)
         {
+            
 
             for (int i = 0; i < 101; i++)
             {
                 (sender as BackgroundWorker).ReportProgress(i);
-                Thread.Sleep(10);
+                Thread.Sleep(3);
             }
+
         }
 
         void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
@@ -56,7 +60,12 @@ namespace PL
             pbStatus.Value = e.ProgressPercentage;
         }
 
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
 
+      
 
     }
 }
