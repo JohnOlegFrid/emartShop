6d812310f6bdfd73c93bed68c7e8cba615c2b56b
arielboss <tobiz7@hotmail.com>
2015-06-04 12:02:25 +0300

    add nunit tests

--- nUnit/Test.cs
+++ nUnit/Test.cs
@@ -14,15 +14,76 @@ namespace nUnit
     //[TestFixture]
     public class Tests
     {
-        private BL_Manager bl_manager;
-        private DAL_Manager dal_manager;
-
-        public Tests() 
+        // [Test] // #1
+        public void checkExistsUser()
+        //@ Add a user and check if he existing
         {
-            //bl_manager = new BL_Manager(dal_manager);
-            //dal_manager = new DAL_Manager();
+            User us = new User("israel", "pw", "12345");
+            User_Data ud = new User_Data();
+            ud.Add(us);
+            //Assert.IsTrue(ud.Contains(us));
         }
 
+
+        //  [Test] //#2
+        public void checkExistsProduct()
+        //@ Add a product, remove it and check if he not existing anymore
+        {
+            Product pr = new Product("creme", Product.Type.cloths, "1234", "456", true, 10, 50);
+            Product_Data pd = new Product_Data();
+            pd.Add("creme", Product.Type.cloths, "1234", "456", true, 10, 50); // we add pd to the list
+            pd.Remove("1234"); // we remove him from the list
+            //Assert.IsTrue(!pd.Contains("1234")); // we're checking that is inventoryId is not existing
+        }
+
+        // [Test] // #3
+        public void checkExistsUser2()
+        //@ Add a user and check if he existing
+        {
+            User us = new User("boli", "pw2", "67891");
+            User_Data ud = new User_Data();
+            ud.Add(us);
+            //Assert.IsTrue(ud.Contains(us));
+        }
+
+        // [Test] //#3
+        public void checkUpdateCM()
+        //@ Add a ClubMember, and check if we can update him
+        {
+            Club_Member cm = new Club_Member("1234", "avi", "dahan", "male", "5618", "2105");
+            ClubMember_Data cd = new ClubMember_Data();
+            cd.Add(cm);
+            ClubMember_BL cb = new ClubMember_BL(cd);
+            //Assert.IsTrue(cb.updateClubMember("5618", "leo", "messi", "male", "2705"));
+        }
+
+
+        // [Test] // #5
+        public void checkDepartment()
+        { //@ Add a deparment and check if he existing
+            Department dp = new Department("bilou", "5874");
+            Department_Data dt = new Department_Data();
+            dt.Add("bilou", "5874");
+            //Assert.IsTrue(dt.Contains(dp));
+        }
+
+        // [Test] // #6
+        public void checkClubMember()
+        { //@ Add a clubMember and check if he existing
+            Club_Member cm = new Club_Member("1234", "avi", "dahan", "male", "5618", "2105");
+            ClubMember_Data cd = new ClubMember_Data();
+            cd.Add(cm);
+            //Assert.IsTrue(cd.Contains(cm));
+        }
+
+        // [Test] // #7
+        public void checkDepartment()
+        { //@ Add a department and check if he existing
+            Department dp = new Department("food", "1212");
+            Department_Data dt = new Department_Data();
+            dt.Add("food", "1212");
+            //Assert.IsTrue(dt.Contains(dp));
+        }
 
 
 
