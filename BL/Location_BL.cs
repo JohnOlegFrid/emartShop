b0a8d446ed1ddc22e9feaf9b72acec6c2a8fcf66
oleg <olegfrid@post.bgu.ac.il>
2015-06-15 18:55:09 +0300

    map edition

---
+++ BL/Location_BL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq;
+using DAL;
+
+namespace BL
+{
+    public class Location_BL
+    {
+        EmartDataContext sqlDB;
+
+        public Location_BL()
+        {
+            sqlDB = new EmartDataContext();
+        }
+
+       
+    }
+}
